9eaed3551947ec6b02e9c06c5ad3814ed1305346
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-04-30 03:06:34 +0530

    refactor: edit structure for query fields and code formate by prettier

---
+++ src/queryAndMutation/query.js
@@ -0,0 +1,16 @@
+const { userType } = require("../types/user");
+const { userRegistration } = require("../models/user");
+const { GraphQLList } = require("graphql");
+
+class Query {
+  users = {
+    type: new GraphQLList(userType),
+    resolve: function () {
+      const users = userRegistration.find().exec();
+      if (users) {
+        return users;
+      }
+    },
+  };
+}
+module.exports = new Query()
--- src/queryAndMutation/user.js
+++ src/queryAndMutation/user.js
@@ -1,30 +1,15 @@
-const {
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLList,
-} = require("graphql");
-const { userRegistration } = require("../models/user");
-const { userType } = require("../types/user");
-const { addUser, loginUser, forgetPass, resetPass } = require("./index")
+const { GraphQLSchema, GraphQLObjectType } = require("graphql");
+const { addUser, loginUser, forgetPass, resetPass } = require("./mutation");
+const { users } = require("./query")
 
 const Query = new GraphQLObjectType({
   name: "GetData",
-  fields: () => ({
-    users: {
-      type: new GraphQLList(userType),
-      resolve: function () {
-        const users = userRegistration.find().exec();
-        if (users) {
-          return users
-        }
-      },
-    },
-  }),
+  fields: { users },
 });
 
 const Mutation = new GraphQLObjectType({
   name: "Registration",
-  fields: { addUser, loginUser, forgetPass, resetPass }
+  fields: { addUser, loginUser, forgetPass, resetPass },
 });
 
 module.exports = new GraphQLSchema({
