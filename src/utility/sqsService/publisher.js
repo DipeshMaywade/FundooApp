64e87abbe83122c5ed1e60bde7d43898481b7da5
unknown <dipeshmaywade@gmail.com>
2021-05-21 10:48:45 +0530

    feat: sent message object to SQS queue

---
+++ src/utility/sqsService/publisher.js
@@ -0,0 +1,41 @@
+const AWS = require('../../../config/awsConfig');
+const logger = require('../logger');
+require('dotenv').config();
+
+// Create an SQS service object
+const queueUrl = process.env.SQS_URL;
+
+module.exports = sentToSQS = (email, token) => {
+  let data = {
+    userEmail: email,
+    token: token,
+  };
+
+  let sqsData = {
+    MessageAttributes: {
+      userEmail: {
+        DataType: 'String',
+        StringValue: email,
+      },
+      token: {
+        DataType: 'String',
+        StringValue: token,
+      },
+    },
+    MessageBody: JSON.stringify(data),
+    MessageDeduplicationId: email,
+    MessageGroupId: 'UserOrders',
+    QueueUrl: queueUrl,
+  };
+
+  // Send the order data to the SQS queue
+  let sendSqsMessage = AWS.sqs.sendMessage(sqsData).promise();
+
+  sendSqsMessage
+    .then((data) => {
+      logger.log(`info: ${data.MessageId}`);
+    })
+    .catch((err) => {
+      logger.log(`error: ${err}`);
+    });
+};
