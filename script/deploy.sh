50c382cac199463002bde56ca83a374d772f6509
unknown <dipeshmaywade@gmail.com>
2021-05-25 12:27:01 +0530

    update: node version change in deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,7 +4,7 @@ ssh ubuntu@18.188.45.202<<EOF
     git pull origin master
     curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh    | bash
     . ~/.nvm/nvm.sh
-    nvm install v12.1.0
+    nvm install v12.0.0
     npm install
     npm install -g nodemon pm2
     pm2 restart ecosystem.config.js
